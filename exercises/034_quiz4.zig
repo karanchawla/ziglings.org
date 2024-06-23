2e0c3317a75e68d8d26e4d5062265f6cc4ff0b5b
Karan Chawla <karan@elroyair.com>
2024-06-23 11:06:24 -0600

    Working through learning zig, current progress: 035_enums.zig

---
+++ exercises/034_quiz4.zig
@@ -0,0 +1,29 @@
+//
+// Quiz time. See if you can make this program work!
+//
+// Solve this any way you like, just be sure the output is:
+//
+//     my_num=42
+//
+const std = @import("std");
+
+const NumError = error{IllegalNumber};
+
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+
+    var my_num: u32 = undefined;
+    if (getNumber()) |value| {
+        my_num = value;
+    } else |err| switch (err) {
+        NumError.IllegalNumber => my_num = 42,
+    }
+
+    try stdout.print("my_num={}\n", .{my_num});
+}
+
+// This function is obviously weird and non-functional. But you will not be changing it for this quiz.
+fn getNumber() NumError!u32 {
+    if (false) return NumError.IllegalNumber;
+    return 42;
+}
